e17cfd21771510c3775b163207e08c04ab2ada12
SouravDev2022 <sourav_dev@outlook.com>
2024-11-28 16:06:30 +0530

    Second Commit

---
+++ src/main/java/com/docker/demo/docker_jenkins_integration_sample/controller/SampleController.java
@@ -0,0 +1,7 @@
+package com.docker.demo.docker_jenkins_integration_sample.controller;
+
+public class SampleController {
+    public void examleMethod(){
+        System.out.println("Example Method Called");
+    }
+}
